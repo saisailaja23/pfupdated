f5f9730313e29e8a26f825b7c5c3a0fee535da0c
dhanyap <dhanya.p@breezegoindia.com>
2016-02-29 09:03:30 +0530

    Update EthnicityService.php

--- LaravelApi/app/Services/EthnicityService.php
+++ LaravelApi/app/Services/EthnicityService.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use App\Models\Profiles;
 use App\Models\Ethnicity;
 
-use App\Repository\ProfileRepository;
+use App\Repository\EthnicityRepository;
 
 /**
  * Description of ParentService
