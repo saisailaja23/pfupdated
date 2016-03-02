2969e7a8c41b3ab3a39367d89bba3bd73d57806c
rahulrpbgi <p.rahul@breezegoindia.com>
2016-03-02 11:12:59 +0530

    testing

--- pfcomm_apiNewTables/table_changes.sql
+++ pfcomm_apiNewTables/table_changes.sql
@@ -8,3 +8,10 @@
 -- PHP Version: 7.0.1
 
 ALTER TABLE `profiles` CHANGE `waiting` `waiting_id` INT(5) NOT NULL;
+ALTER TABLE `Countries` CHANGE `Name` `country` VARCHAR(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
+ ALTER TABLE `Countries` CHANGE `Code` `country_code` VARCHAR(3) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
+ ALTER TABLE `Countries` CHANGE `Id` `country_id` INT(5) NOT NULL AUTO_INCREMENT;
+ ALTER TABLE `States` CHANGE `Country` `country_id` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, CHANGE `CountryCode` `CountryCode` VARCHAR(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+  ALTER TABLE `States` DROP `CountryCode`;
+   ALTER TABLE `States` ADD `state_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
+    ALTER TABLE `States` ADD `accounts_id` INT NOT NULL ;
