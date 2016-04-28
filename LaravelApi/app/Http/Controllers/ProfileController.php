a99effba1d4321b38f7d3ff409b61ab2c0a8b624
dhanyap <dhanya.p@breezegoindia.com>
2016-04-28 11:36:05 +0530

    New changes

--- LaravelApi/app/Http/Controllers/ProfileController.php
+++ LaravelApi/app/Http/Controllers/ProfileController.php
@@ -559,7 +559,7 @@ class ProfileController extends Controller
           {
            
           	$countrysdetails=array("status"=>"200",
-          							"Country Details"=>$countrys);
+          							"countryDetails"=>$countrys);
             return $_GET['callback']."(".json_encode($countrysdetails).")";
            }
            else
@@ -575,7 +575,7 @@ class ProfileController extends Controller
        	   $stateObj=new UtilityService;
        	   if($states=$stateObj->getStatesByCountryId($country_id))
            {
-       	   $stateDetails=Array("status"=>"200","State Details"=>$states);
+       	   $stateDetails=Array("status"=>"200","stateDetails"=>$states);
            return $_GET['callback']."(".json_encode($stateDetails).")";
            }
           else
@@ -592,7 +592,7 @@ class ProfileController extends Controller
         	$region=$regionObj->getRegionDetails();
         	if($region!="")
         	{
-        	$regionDetail=Array("status"=>"200","Region Details"=>$region);
+        	$regionDetail=Array("status"=>"200","regionDetails"=>$region);
         	return $_GET['callback']."(".json_encode($regionDetail).")";
            }
            else
@@ -1195,7 +1195,7 @@ public function getChildren(){
 				}
                     
     }
-public function postChildPhoto(Request $request){
+	public function postChildPhoto(Request $request){
     	$data['child_id']=verifyData($request->child_id);
     	$data['title']=verifyData($request->title);
 	    $data['url']=verifyData($request->url);
@@ -1213,8 +1213,8 @@ public function postChildPhoto(Request $request){
         	$religion=$religionObj->getReligion();
         	if(count($religion)!="")
         	{
-        	$religionDetail=Array("status"=>"200","Religion Details"=>$religion);
-        	return json_encode($religionDetail);
+        	$religionDetail=Array("status"=>"200","religionDetails"=>$religion);
+        	return $_GET['callback']."(".json_encode($religionDetail).")";
            }
            else
            {
@@ -1225,7 +1225,7 @@ public function postChildPhoto(Request $request){
  public function getKidsApi()
     {
     	$kidsObj=new UtilityService;
-        	$kids=$kidsObj->getKids();
+        $kids=$kidsObj->getKids();
     }
 
 
